06cf1133ba90cf0ad608adcd8ec4603b765d459d
Phillip Kerman <phillipkerman@gmail.com>
2018-10-11 13:05:26 -0700

    plain ws

--- express/server.js
+++ express/server.js
@@ -6,11 +6,10 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  //res.writeHead(200, { 'Content-Type': 'text/html' });
- // res.write('<h1>is express</h1>');
- // res.send()
-   res.sendFile('/chat.html');
- // res.end();
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h2>want to: res.sendFile("/chat.html");</h2>');
+  res.send()
+  res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
@@ -18,7 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-
+/*
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 io.on('connection', function(socket){
@@ -34,7 +33,32 @@ io.on('connection', function(socket){
       console.log( loc )
   })
 });
+*/
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ port: 8080 });
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+wss.on('connection', function connection(ws) {
+
+
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
