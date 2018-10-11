071d0c48b6ff07ca392fa90ea3f120a78ea18241
Phillip Kerman <phillipkerman@gmail.com>
2018-10-11 10:55:31 -0700

    more

--- express/server.js
+++ express/server.js
@@ -6,10 +6,11 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+ /* res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>working</h1>');
+  */
   res.sendFile(__dirname + '/chat.html');
-  res.end();
+  //res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
